f7fe59da8e36d7d1613d9734463729d2c0f26616
taocore@gmail.com <taocore@gmail.com@994f2f9b-4fb9-37a0-3060-a1c2e6684766>
2011-11-23 05:30:13 +0000

    refactor
    git-svn-id: http://tuqianyi3.googlecode.com/svn/tuqianyi3@127 994f2f9b-4fb9-37a0-3060-a1c2e6684766

--- src/com/tuqianyi/action/MergeAction.java
+++ src/com/tuqianyi/action/MergeAction.java
@@ -16,7 +16,6 @@ import javax.imageio.ImageIO;
 
 import org.apache.commons.lang.StringUtils;
 
-import com.opensymphony.xwork2.ActionContext;
 import com.taobao.api.ApiException;
 import com.taobao.api.response.ItemGetResponse;
 import com.taobao.api.response.ItemUpdateResponse;
@@ -39,29 +38,30 @@ public class MergeAction extends ActionBase {
 	private ImageLabel frame;
 	
 	public String execute() throws Exception {
-		Connection conn = null;
-		try
+		if (numIids != null)
 		{
-			if (numIids != null)
+			final String[] iids = StringUtils.split(numIids, ',');
+			if (!checkItemsCount(iids.length))
 			{
-				String[] iids = StringUtils.split(numIids, ',');
-				if (!checkItemsCount(iids.length))
-				{
-					return Constants.OUT_OF_ALLOWED_ITEMS;
-				}
-				updateProgress(iids.length, 0);
-				conn = DBUtils.getConnection();
-				List<Item> items = getItems(iids, conn);
-				merge(items, merges, conn);
+				return Constants.OUT_OF_ALLOWED_ITEMS;
 			}
-		}
-		catch (Exception e)
-		{
-			error(e);
-		}
-		finally
-		{
-			DBUtils.close(conn, null, null);
+			updateProgress(iids.length, 0);
+			Connection conn = null;
+			try
+			{
+				conn = DBUtils.getConnection();
+				final List<Item> items = getItems(iids, conn);
+				merging(items, conn);
+				merge(items, frame, merges);
+			}
+			catch (Exception e)
+			{
+				error(e);
+			}
+			finally
+			{
+				DBUtils.close(conn, null, null);
+			}
 		}
 		return SUCCESS;
 	}
@@ -103,285 +103,53 @@ public class MergeAction extends ActionBase {
 		return items;
 	}
 	
-	public String merge(List<Item> items, List<Merge> merges, Connection conn)
+	private void merging(List<Item> items, final Connection conn)
 	{
-		String topSession = getSessionId();
-		try
+		for (Item item : items)
 		{
-			for (Item item : items)
+			if (item.getStatus() != Item.STATUS_NORMAL)
 			{
-				if (item.getStatus() == Item.STATUS_OK)
-				{
-					continue;
-				}
-				try {
-					Dao.INSTANCE.merging(item, getUser(), item.getPicUrl(), -1L, conn);
-				} catch (Exception e) {
-					_log.log(Level.SEVERE, "", e);
-				}
+				continue;
 			}
-		
-			int count = items.size();
-			int processed = 0;
-			updateProgress(count, processed);
-			for (Item item : items)
-			{
-				if (item.getStatus() == Item.STATUS_OK)
-				{
-					processed++;
-					updateProgress(count, processed);
-					continue;
-				}
-				merge(item, merges, topSession, conn);
-				processed++;
-				updateProgress(count, processed);
-				System.gc();
-			}
-		}
-		catch (Exception e)
-		{
-			_log.log(Level.SEVERE, "", e);
-		}
-		return null;
-	}
-	
-	private void merge(Item item, List<Merge> merges, String topSession, Connection conn)
-	{
-		try {
-			_log.info("merging..." + item.getNumIid());
-			String picUrl = item.getPicUrl();
-			if (picUrl == null)
-			{
-				_log.info("no pic: " + item.getNumIid());
-				return;
-			}
-			BufferedImage image = ImageIO.read(new URL(picUrl));
-			String rootPath = FontsServlet.getRootPath();
-			if (frame != null)
-			{
-				image = addFrame(image, rootPath);
-			}
-			if (merges != null)
-			{
-				for (Merge m : merges)
-				{
-					image = mergeImage(image, m, item);
-				}
-			}
-			ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-			try
-			{
-				ImageUtils.writeImage(image, "jpg", 1, out2);
-				int size = out2.size();
-				if (size > 512000)
-				{
-					out2 = new ByteArrayOutputStream();
-					ImageUtils.writeImage(image, "jpg", 512000F/size, out2);
-				}
-				_log.info("size: " + out2.size());
-				ItemUpdateResponse response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), out2.toByteArray());
-				if (response.isSuccess())
-				{
-					com.taobao.api.domain.Item updatedItem = response.getItem();
-					updatedItem.setPicUrl(getNewPicUrl(topSession, item.getNumIid()));
-					Dao.INSTANCE.merged(item.getNumIid(), updatedItem.getPicUrl(), updatedItem.getModified(), Item.STATUS_OK, null, null, conn);
-				}
-				else
-				{
-					_log.info(response.getErrorCode() + ": " +response.getMsg() + ", " + response.getSubCode() + ": " + response.getSubMsg());
-					String errorMsg = response.getSubMsg() == null ? response.getMsg() : response.getSubMsg();
-					if ("isv.item-is-delete:invalid-numIid-or-iid".equals(response.getSubCode()) || "isv.item-get-service-error:ITEM_NOT_FOUND".equals(response.getSubCode()))
-					{
-						Dao.INSTANCE.deleteMergedItem(item.getNumIid(), conn);
-					}
-					else
-					{
-						if ("isv.item-update-service-error:IC_ITEM_PIC_IS_TOO_LARGES".equals(response.getSubCode()))
-						{
-							errorMsg = "合成后的图像文件大小超过淘宝限制的最大值500K";
-						}
-						Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, errorMsg, response.getSubCode(), conn);
-					}
-				}
-			} 
-			catch (ApiException e) {
+			try {
+				Dao.INSTANCE.merging(item, getUser(), item.getPicUrl(), -1L, conn);
+			} catch (Exception e) {
 				_log.log(Level.SEVERE, "", e);
 			}
-			catch (Exception e)
-			{
-				_log.log(Level.SEVERE, "", e);
-				try {
-					Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
-				} catch (Exception e1) {
-					_log.log(Level.SEVERE, "", e1);
-				}
-			}
-			finally
-			{
-				out2.close();
-			}
-		} catch (MalformedURLException e) {
-			_log.log(Level.SEVERE, "", e);
-			try {
-				Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
-			} catch (Exception e1) {
-				_log.log(Level.SEVERE, "", e1);
-			}
-		} catch (IOException e) {
-			_log.log(Level.SEVERE, "", e);
-			try {
-				Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
-			} catch (Exception e1) {
-				_log.log(Level.SEVERE, "", e1);
-			}
 		}
 	}
 	
-	private BufferedImage createMergeImage(Merge m, Item item) throws IOException
+	public String merge(List<Item> items, ImageLabel frame, final List<Merge> merges)
 	{
-		ImageLabel imageLabel = m.getImageLabel();
-		TextLabel textLabel = m.getTextLabel();
-		if (imageLabel != null)
+		int count = items.size();
+		updateProgress(count, 0);
+		for (final Item item : items)
 		{
-			if (m.getImage() == null)
+			if (item.getStatus() != Item.STATUS_NORMAL)
 			{
-				BufferedImage labelImage = null;
-				_log.info("label id: " + imageLabel.getId());
-				if (ImageLabel.isLocal(imageLabel.getSrc()))
-				{
-					String root = FontsServlet.getRootPath();
-					_log.info("label file path: " + root + imageLabel.getSrc());
-					File f = new File(root + imageLabel.getSrc());
-					labelImage = ImageIO.read(f);
-				}
-				else
-				{
-					URL labelUrl = new URL(imageLabel.getSrc());
-					labelImage = ImageIO.read(labelUrl);
-				}
-				m.setImage(labelImage);
+				increaseProgress();
+				continue;
 			}
-			return m.getImage();
-		}
-		else if (textLabel != null)
-		{
-			if (textLabel.hasToken())
-			{
-				String text = textLabel.getParseText(item.getPrice());
-				String color = textLabel.getColor();
-				String backColor = textLabel.getBackground();
-				return FontProvider.getInstance().createText(text, textLabel.getFont(), color, backColor, textLabel.getStyle(), textLabel.getLine(), textLabel.getBorderWidth(), textLabel.getAngle());
-			}
-			else
-			{
-				Map<String, Object> session = ActionContext.getContext().getSession();
-				return (BufferedImage)session.get(textLabel.getId());
-			}
+			Runnable task = new MergeTask(item, frame, merges, getSession());
+//			executeInPool(task);
+			task.run();
+			increaseProgress();
 		}
 		return null;
-	}
-	
-	private String getNewPicUrl(String topSession, long numIid)
-	{
-		try {
-			ItemGetResponse response = TaobaoProxy.getItem(topSession, numIid);
-			if (response.isSuccess())
-			{
-				return response.getItem().getPicUrl();
-			}
-			else
-			{
-				_log.info(response.getErrorCode() + ": " +response.getMsg());
-				_log.info(response.getSubCode() + ": " + response.getSubMsg());
-			}
-		} catch (ApiException e) {
-			_log.log(Level.SEVERE, "", e);
-		}
-		return null;
-	}
-	
-	private BufferedImage mergeImage(BufferedImage image, Merge m, Item item) throws IOException
-	{
-		BufferedImage labelImage = createMergeImage(m, item);
-		if (labelImage == null)
-		{
-			_log.warning("No label image was found.");
-			return image;
-		}
-
-		_log.info("m.width: " + m.getWidth());
-		_log.info("m.height: " + m.getHeight());
-		float orignal2PreviewRatio = getOriginal2PreviewRatio(image);
-		int newWidth = Math.round(m.getWidth() * orignal2PreviewRatio);
-		int newHeight = Math.round(m.getHeight() * orignal2PreviewRatio);
-		int newX = Math.round(m.getX() * orignal2PreviewRatio);
-		int newY = Math.round(m.getY() * orignal2PreviewRatio);
-		if (newX + newWidth > image.getWidth())
-		{
-			newX = image.getWidth() - newWidth;
-		}
-		if (newY + newHeight > image.getHeight())
-		{
-			newY = image.getHeight() - newHeight;
-		}
-		_log.info("new width: " + newWidth + ", new height: " + newHeight);
-		_log.info("new x: " + newX + ", new y: " + newY);
-		image = ImageUtils.composite(image, labelImage, newX, newY, newWidth, newHeight, m.getOpacity()/100F);
-		return image;
-	}
-	
-	private BufferedImage addFrame(BufferedImage image, String rootPath) throws IOException
-	{
-		File f = new File(rootPath + frame.getSrc());
-		BufferedImage frameImage = ImageIO.read(f);
-		return ImageUtils.composite(image, frameImage, 0, 0, image.getWidth(), image.getHeight(), 1F);
-	}
-	
-	private float getOriginal2PreviewRatio(BufferedImage originalImage)
-	{
-		if (originalImage.getWidth() >= originalImage.getHeight() && originalImage.getWidth() > 310)
-		{
-			return 1.0f * originalImage.getWidth() / 310;
-		}
-		else if (originalImage.getHeight() >= originalImage.getWidth() && originalImage.getHeight() > 310)
-		{
-			return 1.0f * originalImage.getHeight() / 310;
-		}
-		return 1;
 	}
 
 	private boolean checkItemsCount(int processingItemsCount)
 	{
 		long processedItemsCount;
 		try {
-			processedItemsCount = Dao.INSTANCE.getMergedItemsCount(getUser(), Item.STATUS_OK);
-			String version = getVersion();
-			_log.info("version: " + version);
-			int allowedItemsCount = getAllowedItems(version);
+			processedItemsCount = Dao.INSTANCE.getMergedItemsCount(getUser());
+			int allowedItemsCount = getAllowedItems();
 			_log.info("allowed items: " + allowedItemsCount);
 			return (processedItemsCount + processingItemsCount <= allowedItemsCount);
 		} catch (Exception e) {
 			_log.log(Level.SEVERE, "", e);
 		}
 		return false;
-	}
-	
-	private int getAllowedItems(String version)
-	{
-		int allowedItemsCount = 10;
-		if ("1".equals(version))
-		{
-			allowedItemsCount = 300;
-		}
-		else if ("2".equals(version))
-		{
-			allowedItemsCount = 800;
-		}
-		else if ("3".equals(version))
-		{
-			allowedItemsCount = 2000;
-		};
-		return allowedItemsCount;
 	}
 
 	public void setNumIids(String numIids) {
@@ -406,5 +174,247 @@ public class MergeAction extends ActionBase {
 
 	public ImageLabel getFrame() {
 		return frame;
+	}
+	
+	private static class MergeTask implements Runnable
+	{
+		private Item item;
+		private ImageLabel frame;
+		private List<Merge> merges;
+		private Map<String, Object> session;
+		
+		public MergeTask(Item item, ImageLabel frame, List<Merge> merges, Map<String, Object> session)
+		{
+			this.item = item;
+			this.frame = frame;
+			this.merges = merges;
+			this.session = session;
+		}
+
+		public void run() {
+			Connection conn = null;
+			try
+			{
+				conn = DBUtils.getConnection();
+				String topSession = (String)session.get(TOP_SESSION);
+				merge(item, frame, merges, topSession, conn);
+			}
+			catch (Exception e)
+			{
+				_log.log(Level.SEVERE, "", e);
+			}
+			finally
+			{
+				DBUtils.close(conn, null, null);
+			}
+		}
+		
+		private void merge(Item item, ImageLabel frame, List<Merge> merges, String topSession, Connection conn)
+		{
+			try {
+				_log.info("merging..." + item.getNumIid());
+				String picUrl = item.getPicUrl();
+				if (picUrl == null)
+				{
+					_log.info("no pic: " + item.getNumIid());
+					return;
+				}
+				BufferedImage image = ImageIO.read(new URL(picUrl));
+				String rootPath = FontsServlet.getRootPath();
+				if (frame != null)
+				{
+					image = addFrame(image, rootPath);
+				}
+				if (merges != null)
+				{
+					for (Merge m : merges)
+					{
+						image = mergeImage(image, m, item);
+					}
+				}
+				ByteArrayOutputStream out2 = new ByteArrayOutputStream();
+				try
+				{
+					ImageUtils.writeImage(image, "jpg", 1, out2);
+					int size = out2.size();
+					if (size > 512000)
+					{
+						out2 = new ByteArrayOutputStream();
+						ImageUtils.writeImage(image, "jpg", 512000F/size, out2);
+					}
+					_log.info("size: " + out2.size());
+					ItemUpdateResponse response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), out2.toByteArray());
+					if (response.isSuccess())
+					{
+						com.taobao.api.domain.Item updatedItem = response.getItem();
+						updatedItem.setPicUrl(getNewPicUrl(topSession, item.getNumIid()));
+						Dao.INSTANCE.merged(item.getNumIid(), updatedItem.getPicUrl(), updatedItem.getModified(), Item.STATUS_OK, null, null, conn);
+					}
+					else
+					{
+						_log.info(response.getErrorCode() + ": " +response.getMsg() + ", " + response.getSubCode() + ": " + response.getSubMsg());
+						String errorMsg = response.getSubMsg() == null ? response.getMsg() : response.getSubMsg();
+						if ("isv.item-is-delete:invalid-numIid-or-iid".equals(response.getSubCode()) || "isv.item-get-service-error:ITEM_NOT_FOUND".equals(response.getSubCode()))
+						{
+							Dao.INSTANCE.deleteMergedItem(item.getNumIid(), conn);
+						}
+						else
+						{
+							if ("isv.item-update-service-error:IC_ITEM_PIC_IS_TOO_LARGES".equals(response.getSubCode()))
+							{
+								errorMsg = "合成后的图像文件大小超过淘宝限制的最大值500K";
+							}
+							Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, errorMsg, response.getSubCode(), conn);
+						}
+					}
+				} 
+				catch (ApiException e) {
+					_log.log(Level.SEVERE, "", e);
+				}
+				catch (Exception e)
+				{
+					_log.log(Level.SEVERE, "", e);
+					try {
+						Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
+					} catch (Exception e1) {
+						_log.log(Level.SEVERE, "", e1);
+					}
+				}
+				finally
+				{
+					out2.close();
+				}
+			} catch (MalformedURLException e) {
+				_log.log(Level.SEVERE, "", e);
+				try {
+					Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
+				} catch (Exception e1) {
+					_log.log(Level.SEVERE, "", e1);
+				}
+			} catch (IOException e) {
+				_log.log(Level.SEVERE, "", e);
+				try {
+					Dao.INSTANCE.merged(item.getNumIid(), null, null, Item.STATUS_FAILED, e.getMessage(), "unknown", conn);
+				} catch (Exception e1) {
+					_log.log(Level.SEVERE, "", e1);
+				}
+			}
+		}
+		
+		private BufferedImage mergeImage(BufferedImage image, Merge m, Item item) throws IOException
+		{
+			BufferedImage labelImage = createMergeImage(m, item);
+			if (labelImage == null)
+			{
+				_log.warning("No label image was found.");
+				return image;
+			}
+
+			_log.info("m.width: " + m.getWidth());
+			_log.info("m.height: " + m.getHeight());
+			float orignal2PreviewRatio = getOriginal2PreviewRatio(image);
+			int newWidth = Math.round(m.getWidth() * orignal2PreviewRatio);
+			int newHeight = Math.round(m.getHeight() * orignal2PreviewRatio);
+			int newX = Math.round(m.getX() * orignal2PreviewRatio);
+			int newY = Math.round(m.getY() * orignal2PreviewRatio);
+			if (newX + newWidth > image.getWidth())
+			{
+				newX = image.getWidth() - newWidth;
+			}
+			if (newY + newHeight > image.getHeight())
+			{
+				newY = image.getHeight() - newHeight;
+			}
+			if (newY < 0)
+			{
+				newY = 0;
+			}
+			_log.info("new width: " + newWidth + ", new height: " + newHeight);
+			_log.info("new x: " + newX + ", new y: " + newY);
+			image = ImageUtils.composite(image, labelImage, newX, newY, newWidth, newHeight, m.getOpacity()/100F);
+			return image;
+		}
+		
+		private BufferedImage addFrame(BufferedImage image, String rootPath) throws IOException
+		{
+			File f = new File(rootPath + frame.getSrc());
+			BufferedImage frameImage = ImageIO.read(f);
+			return ImageUtils.composite(image, frameImage, 0, 0, image.getWidth(), image.getHeight(), 1F);
+		}
+		
+		private String getNewPicUrl(String topSession, long numIid)
+		{
+			try {
+				ItemGetResponse response = TaobaoProxy.getItem(topSession, numIid);
+				if (response.isSuccess())
+				{
+					return response.getItem().getPicUrl();
+				}
+				else
+				{
+					_log.info(response.getErrorCode() + ": " +response.getMsg());
+					_log.info(response.getSubCode() + ": " + response.getSubMsg());
+				}
+			} catch (ApiException e) {
+				_log.log(Level.SEVERE, "", e);
+			}
+			return null;
+		}
+		
+		private BufferedImage createMergeImage(Merge m, Item item) throws IOException
+		{
+			ImageLabel imageLabel = m.getImageLabel();
+			TextLabel textLabel = m.getTextLabel();
+			if (imageLabel != null)
+			{
+				if (m.getImage() == null)
+				{
+					BufferedImage labelImage = null;
+					_log.info("label id: " + imageLabel.getId());
+					if (ImageLabel.isLocal(imageLabel.getSrc()))
+					{
+						String root = FontsServlet.getRootPath();
+						_log.info("label file path: " + root + imageLabel.getSrc());
+						File f = new File(root + imageLabel.getSrc());
+						labelImage = ImageIO.read(f);
+					}
+					else
+					{
+						URL labelUrl = new URL(imageLabel.getSrc());
+						labelImage = ImageIO.read(labelUrl);
+					}
+					m.setImage(labelImage);
+				}
+				return m.getImage();
+			}
+			else if (textLabel != null)
+			{
+				if (textLabel.hasToken())
+				{
+					String text = textLabel.getParseText(item.getPrice());
+					String color = textLabel.getColor();
+					String backColor = textLabel.getBackground();
+					return FontProvider.getInstance().createText(text, textLabel.getFont(), color, backColor, textLabel.getStyle(), textLabel.getLine(), textLabel.getBorderWidth(), textLabel.getAngle());
+				}
+				else
+				{
+					return (BufferedImage)session.get(textLabel.getId());
+				}
+			}
+			return null;
+		}
+		
+		private float getOriginal2PreviewRatio(BufferedImage originalImage)
+		{
+			if (originalImage.getWidth() >= originalImage.getHeight() && originalImage.getWidth() > 310)
+			{
+				return 1.0f * originalImage.getWidth() / 310;
+			}
+			else if (originalImage.getHeight() >= originalImage.getWidth() && originalImage.getHeight() > 310)
+			{
+				return 1.0f * originalImage.getHeight() / 310;
+			}
+			return 1;
+		}
 	}
 }
--- src/com/tuqianyi/action/RecoverAction.java
+++ src/com/tuqianyi/action/RecoverAction.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.sql.Connection;
+import java.util.Map;
 import java.util.logging.Level;
 
 import javax.imageio.ImageIO;
@@ -12,7 +13,6 @@ import javax.imageio.ImageIO;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
-import com.taobao.api.ApiException;
 import com.taobao.api.response.ItemUpdateResponse;
 import com.tuqianyi.db.DBUtils;
 import com.tuqianyi.db.Dao;
@@ -25,33 +25,14 @@ public class RecoverAction extends ActionBase{
 	private String numIids;
 	
 	public String execute() throws Exception {
-		_log.info("numIids: " + numIids);
-		String[] ids = StringUtils.split(numIids, ',');
-		String topSession = getSessionId();
-		int processed = 0;
+		final String[] ids = StringUtils.split(numIids, ',');
+		updateProgress(ids.length, 0);
 		Connection conn = null;
 		try
 		{
 			conn = DBUtils.getConnection();
-			updateProgress(ids.length, processed);
-			for (String id: ids)
-			{
-				Item item = Dao.INSTANCE.getMergedItem(Long.parseLong(id), conn);
-				if (item != null && item.isMerged())
-				{
-					Dao.INSTANCE.recovering(item.getNumIid(), conn);
-					try
-					{
-						recover(item, topSession, conn);
-					}
-					catch (Exception e)
-					{
-						_log.log(Level.SEVERE, "", e);
-						Dao.INSTANCE.unmerged(item.getNumIid(), false, e.getMessage(), conn);
-					}
-				}
-				updateProgress(ids.length, ++processed);
-			}
+			Dao.INSTANCE.recovering(ids, conn);
+			recover(ids);
 		}
 		catch (Exception e) {
 			_log.log(Level.SEVERE, "", e);
@@ -63,89 +44,144 @@ public class RecoverAction extends ActionBase{
 		return SUCCESS;
 	}
 	
-	private static void recover(Item item, String topSession, Connection conn) throws Exception
+	private void recover(String[] ids)
 	{
-			if (item.getOldPicUrl() != null)
-			{
-				String oldPicUrl = item.getOldPicUrl();
-				_log.info("nick: " + getUser() + " num_iid:" + item.getNumIid() + " old: " + oldPicUrl);
-				URL oldUrl = new URL(oldPicUrl);
-				byte[] data = IOUtils.toByteArray(oldUrl.openStream());
-				_log.info("data.length: " + data.length);
-				ItemUpdateResponse response;
-				if (data.length > 524288)
-				{
-					_log.info("recovering by picPath...");
-					String key = "bao/uploaded/";
-					int i = oldPicUrl.indexOf(key);
-					String picPath = oldPicUrl.substring(i + key.length());
-					_log.info("kongjian.url: " + oldPicUrl);
-					response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), picPath);
-					if (!response.isSuccess())
-					{
-						error(response);
-					}
-				}
-				else
-				{
-					_log.info("recovering by data...");
-					response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), data);
-					if (!response.isSuccess())
-					{
-						error(response);
-					}
-				}
-				
-				if (!response.isSuccess() && data.length > 500000)
-				{
-					_log.info("recovering by reduced data...");
-					float quality = 0.9F;
-					byte[] tmp = null;
-					ByteArrayInputStream in = new ByteArrayInputStream(data);
-					BufferedImage image = ImageIO.read(in);
-					for (int i = 0; i < 5; i++)
-					{
-						ByteArrayOutputStream out = new ByteArrayOutputStream();							
-						ImageUtils.writeImage(image, "jpg", quality, out);
-						tmp = out.toByteArray();
-						_log.info("reduced data.length: " + tmp.length);
-						if (tmp.length < 500000)
-						{
-							break;
-						}
-						quality = quality - 0.1F;
-					};
-					if (tmp != null)
-					{
-						response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), tmp);
-					}
-				}
-				
-				if (response.isSuccess())
-				{
-					Dao.INSTANCE.unmerged(item.getNumIid(), true, null, conn);
-				}
-				else
-				{
-					error(response);
-					if ("isv.item-is-delete:invalid-numIid-or-iid".equals(response.getSubCode()) || "isv.item-get-service-error:ITEM_NOT_FOUND".equals(response.getSubCode()))
-					{
-						Dao.INSTANCE.deleteMergedItem(item.getNumIid(), conn);
-					}
-					else
-					{
-						String errorMsg = response.getSubMsg() == null ? response.getMsg() : response.getSubMsg();
-						Dao.INSTANCE.unmerged(item.getNumIid(), false, errorMsg, conn);
-					}
-				}
-			}
+		Map<String, Object> session = getSession();
+		for (String id: ids)
+		{
+			Runnable task = new RecoverTask(id, session);
+//			executeInPool(task);
+			task.run();
+			increaseProgress();
+		}
 	}
-
+	
 	public void setNumIids(String numIids) {
 		this.numIids = numIids;
 	}
 
 	public String getNumIids() {
 		return numIids;
+	}
+	
+	private static class RecoverTask implements Runnable
+	{
+		private String numIid;
+		private Map<String, Object> session;
+		
+		public RecoverTask(String numIid, Map<String, Object> session)
+		{
+			this.numIid = numIid;
+			this.session = session;
+		}
+
+		public void run() {
+			Connection conn = null;
+			try
+			{
+				conn = DBUtils.getConnection();
+				Item item = Dao.INSTANCE.getMergedItem(Long.parseLong(numIid), conn);
+				String topSession = (String)session.get(TOP_SESSION);
+				if (item != null && item.isMerged())
+				{
+					recover(item, topSession, conn);
+				}
+			}
+			catch (Exception e)
+			{
+				_log.log(Level.SEVERE, "", e);
+			}
+			finally
+			{
+				DBUtils.close(conn, null, null);
+			}
+		}
+		
+		private void recover(Item item, String topSession, Connection conn) throws Exception
+		{
+			try
+			{
+				if (item.getOldPicUrl() != null)
+				{
+					String oldPicUrl = item.getOldPicUrl();
+					String user = (String)session.get(USER);
+					_log.info("nick: " + user + " num_iid:" + item.getNumIid() + " old: " + oldPicUrl);
+					URL oldUrl = new URL(oldPicUrl);
+					byte[] data = IOUtils.toByteArray(oldUrl.openStream());
+					_log.info("data.length: " + data.length);
+					ItemUpdateResponse response;
+					if (data.length > 524288)
+					{
+						_log.info("recovering by picPath...");
+						String key = "bao/uploaded/";
+						int i = oldPicUrl.indexOf(key);
+						String picPath = oldPicUrl.substring(i + key.length());
+						_log.info("kongjian.url: " + oldPicUrl);
+						response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), picPath);
+						if (!response.isSuccess())
+						{
+							error(response);
+						}
+					}
+					else
+					{
+						_log.info("recovering by data...");
+						response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), data);
+						if (!response.isSuccess())
+						{
+							error(response);
+						}
+					}
+					
+					if (!response.isSuccess() && data.length > 500000)
+					{
+						_log.info("recovering by reduced data...");
+						float quality = 0.9F;
+						byte[] tmp = null;
+						ByteArrayInputStream in = new ByteArrayInputStream(data);
+						BufferedImage image = ImageIO.read(in);
+						for (int i = 0; i < 5; i++)
+						{
+							ByteArrayOutputStream out = new ByteArrayOutputStream();							
+							ImageUtils.writeImage(image, "jpg", quality, out);
+							tmp = out.toByteArray();
+							_log.info("reduced data.length: " + tmp.length);
+							if (tmp.length < 500000)
+							{
+								break;
+							}
+							quality = quality - 0.1F;
+						};
+						if (tmp != null)
+						{
+							response = TaobaoProxy.updateMainPic(topSession, item.getNumIid(), tmp);
+						}
+					}
+					
+					if (response.isSuccess())
+					{
+						Dao.INSTANCE.unmerged(item.getNumIid(), true, null, conn);
+					}
+					else
+					{
+						error(response);
+						if ("isv.item-is-delete:invalid-numIid-or-iid".equals(response.getSubCode()) || "isv.item-get-service-error:ITEM_NOT_FOUND".equals(response.getSubCode()))
+						{
+							Dao.INSTANCE.deleteMergedItem(item.getNumIid(), conn);
+						}
+						else
+						{
+							String errorMsg = response.getSubMsg() == null ? response.getMsg() : response.getSubMsg();
+							Dao.INSTANCE.unmerged(item.getNumIid(), false, errorMsg, conn);
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				_log.log(Level.SEVERE, "", e);
+				Dao.INSTANCE.unmerged(item.getNumIid(), false, e.getMessage(), conn);
+			}
+		}
 	}
 }
